8c0d1ea2367ec60b2b386329c9723a7e14091fa2
JStivenpgSGM <jopachon@personalsoft.com.co>
2021-06-18 15:43:01 -0500

    First Class.

---
+++ views/01.js
@@ -0,0 +1,12 @@
+const app = new Vue({
+    el: '#app',
+    data: {
+        titulo: 'Hola Mundo, trabajando con VueJs.',
+        producto: [
+            {nombre: 'Manzana', cantidad: 15},
+            {nombre: 'Pera', cantidad: 0},
+            {nombre: 'Fresa', cantidad: 7}
+        ]
+    }
+})
+
